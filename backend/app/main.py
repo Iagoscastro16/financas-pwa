9176dbe3d42e16fba2f35643fefa5c6f9a6f24fb
Iagoscastro16 <Iagoscastro16@gmail.com>
2026-08-27 22:43:31 -0300

    Corrige centralização do month selector

--- backend/app/main.py
+++ backend/app/main.py
@@ -51,7 +51,7 @@ app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173","http://192.168.0.8:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
